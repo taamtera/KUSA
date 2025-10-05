993318abb0b25b5b905523da023ff5f56b225e2a
Taam Paramee <taamtera@gmail.com>
2025-10-05 15:51:47 +0700

    fix merge

--- frontend/app/(dashboard)/profile/page.jsx
+++ frontend/app/(dashboard)/profile/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 
+import React, { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
