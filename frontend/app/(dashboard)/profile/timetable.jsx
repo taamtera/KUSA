a3f68c07e679d20ae30bbdabb819d09ad240006e
Chaiyapat Kumtho <scienceteam48036@gmail.com>
2025-11-04 23:20:54 +0700

    commit before pull

--- frontend/app/(dashboard)/profile/timetable.jsx
+++ frontend/app/(dashboard)/profile/timetable.jsx
@@ -67,8 +67,8 @@ export default function ProfilePage({ user }) {
             color: s.color || 'purple',
             gridColumn: `${startCol} / span ${spanCols}`,
             gridRow: row,
-            startMin: s.start_min / 60,
-            endMin: s.end_min / 60,
+            startMin: s.start_min / 60 - 1,
+            endMin: s.end_min / 60 - 1,
             day: s.day
         };
     });
@@ -81,16 +81,6 @@ export default function ProfilePage({ user }) {
         // Wait 100ms before setting state
         colorTimerRef.current = setTimeout(() => {
             setColor(newColor);
-        }, 100);
-    };
-    const handleColorChangeOnEdit = (e) => {
-        const newColor = e.target.value;
-        // Clear any previous pending updates
-        clearTimeout(colorTimerRef.current);
-
-        // Wait 100ms before setting state
-        colorTimerRef.current = setTimeout(() => {
-            setEditColor(newColor);
         }, 100);
     };
 
@@ -110,8 +100,8 @@ export default function ProfilePage({ user }) {
                     title: editTitle,
                     description: editDescription,
                     day: editDay,
-                    start_min: (Number(editStartMin)) * 60,
-                    end_min: (Number(editEndMin)) * 60,
+                    start_min: (Number(editStartMin) + 1) * 60,
+                    end_min: (Number(editEndMin) + 1) * 60,
                     color: editColor,
                 }),
                 credentials: "include",
@@ -149,8 +139,8 @@ export default function ProfilePage({ user }) {
                         title,
                         description,
                         day,
-                        start_min: (Number(start_min)) * 60,
-                        end_min: (Number(end_min)) * 60,
+                        start_min: (Number(start_min) + 1) * 60,
+                        end_min: (Number(end_min) + 1) * 60,
                         location,
                         color
                     }),
@@ -183,131 +173,13 @@ export default function ProfilePage({ user }) {
     };
 
     return (
-        <div className="relative px-8 pb-4 h-auto w-full">
-
-        <div className="flex flex-col mt-8 mb-4">
-            <h2 className="text-xl font-semibold">
-                Timetable
-            </h2>
-
-            {/* Share Button */}
-            <Button className="absolute px-4 right-10 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded w-15">share</Button>
-            {/* Save Button */}
-            <Button className="absolute px-4 right-30 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded w-15">save</Button>
-            
-            {/* Add Button */}
-            <Dialog open={open} onOpenChange={setOpenAdd}>
-                <DialogTrigger asChild>
-                    <Button className="absolute right-70 px-4 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded w-15">add</Button>
-                </DialogTrigger>
-                <DialogContent className="max-w-sm bg-white text-black">
-                    <DialogHeader>
-                        <DialogTitle>Add to Timetable</DialogTitle>
-                    </DialogHeader>
-                    <form className="w-full max-w-sm space-y-4" onSubmit={handleSubmitAdd}>
-                        <div>
-                            <label>Class name</label>
-                            <Input
-                                className="w-full border rounded p-2"
-                                value={title}
-                                onChange={(e) => setTitle(e.target.value)}
-                            />
-                        </div>
-                        <div>
-                            <label>Description</label>
-                            <Input className="w-full border rounded p-2"
-                                value={description}
-                                onChange={(e) => setDescription(e.target.value)}
-                            />
-                        </div>
-                        <div className="relative w-full max-w-sm">
-                            <label>Day</label>
-                            <Button
-                                className="rounded-[4px] border-2 border-gray-100 justify-between text-gray-800 hover:text-gray-800 hover:bg-gray-200 shadow-md bg-transparent w-full"
-                                onClick={() => setopenAddDrop(!openAddDrop)} // toggle dropdown
-                                type="button"
-                            >
-                                {day ? (DaysList.find(d => d.value === day)?.label || "Select day") : "Select day"}
-                                <svg
-                                    viewBox="0 0 16 16"
-                                    fill="currentColor"
-                                    aria-hidden="true"
-                                    className="size-5 text-gray-400 sm:size-4"
-                                >
-                                    <path
-                                        d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z"
-                                        clipRule="evenodd"
-                                        fillRule="evenodd"
-                                    />
-                                </svg>
-                            </Button>
-
-                            {openAddDrop && (
-                                <ul className="absolute text-gray-700 pt-1 shadow-md w-full rounded-[4px] bg-gray-200 z-10">
-                                    {DaysList.map((d) => (
-                                        <li key={d.value}>
-                                            <button
-                                                type="button"
-                                                onClick={() => {
-                                                    setDay(d.value);
-                                                    setopenAddDrop(false);
-                                                }}
-                                                className="hover:bg-gray-300 bg-gray-200 w-full text-left pt-1 px-3 pb-1 rounded-[4px] justify-between items-center flex"
-                                            >
-                                                <p>{d.label}</p>
-                                            </button>
-                                        </li>
-                                    ))}
-                                </ul>
-                            )}
-                        </div>
-                        <div className="flex gap-4">
-                            <div className="flex-1">
-                                <label>Time (24 Hr format)</label>
-                                <Input className="w-full border rounded p-2"
-                                    placeholder="From"
-                                    value={start_min}
-                                    onChange={(e) => setStart_min(e.target.value)}
-                                />
-                            </div>
-                            <div className="flex-1">
-                                <label className="invisible">To</label>
-                                <Input className="w-full border rounded p-2"
-                                    placeholder="To"
-                                    value={end_min}
-                                    onChange={(e) => setEnd_min(e.target.value)}
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <div>
-                                <label for="hs-color-input">Color</label> <p className="text-gray-200"></p>
-                                <input type="color"
-                                    class="p-1 h-14 w-full block bg-white border border-gray-200 cursor-pointer rounded disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700"
-                                    id="hs-color-input"
-                                    value={color}
-                                    title="Choose your color"
-                                    onChange={handleColorChange}
-                                ></input>
-                            </div>
-                        </div>
-                        <div className="flex justify-end gap-2 pt-4">
-                            <Button type="button" variant="outline" className="cursor-pointer bg-transparent" onClick={() => setOpenAdd(false)}>
-                                Cancel
-                            </Button>
-                            <Button type="submit" className="cursor-pointer">
-                                Save
-                            </Button>
-                        </div>
-                    </form>
-                </DialogContent>
-            </Dialog>
-
-            {/* Edit Button */}
+        <div className="">
+            <Button className="absolute py-3 mt-5 px-4 right-10 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded">share</Button>
+            <Button className="absolute py-3 mt-5 px-4 right-30 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded">save</Button>
             {/* <Button className="absolute py-3 mt-5 px-4 right-50 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded">edit</Button> */}
             <Dialog open={openEdit} onOpenChange={setOpenEdit}>
                 <DialogTrigger asChild>
-                    <Button className="absolute right-50 px-4 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded w-15">
+                    <Button className="absolute py-3 mt-5 px-4 right-50 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded">
                         edit
                     </Button>
                 </DialogTrigger>
@@ -426,7 +298,7 @@ export default function ProfilePage({ user }) {
                                         type="color"
                                         className="p-1 h-14 w-full block bg-white border border-gray-200 rounded"
                                         value={editColor}
-                                        onChange={(e) => handleColorChangeOnEdit(e, setEditColor)}
+                                        onChange={(e) => setEditColor(e.target.value)}
                                     />
                                 </div>
                             </>
@@ -446,10 +318,116 @@ export default function ProfilePage({ user }) {
                     </form>
                 </DialogContent>
             </Dialog>
-        </div>
 
             {/* TimeTable Session */}
-            <TimeTableGrid propUserId={user._id}/>
+            <Dialog open={open} onOpenChange={setOpenAdd}>
+                <DialogTrigger asChild>
+                    <Button className="absolute py-3 mt-5 px-4 right-70 cursor-pointer bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white border border-blue-500 hover:border-transparent rounded">add</Button>
+                </DialogTrigger>
+                <DialogContent className="max-w-sm bg-white text-black">
+                    <DialogHeader>
+                        <DialogTitle>Add to Timetable</DialogTitle>
+                    </DialogHeader>
+                    <form className="w-full max-w-sm space-y-4" onSubmit={handleSubmitAdd}>
+                        <div>
+                            <label>Class name</label>
+                            <Input
+                                className="w-full border rounded p-2"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <label>Description</label>
+                            <Input className="w-full border rounded p-2"
+                                value={description}
+                                onChange={(e) => setDescription(e.target.value)}
+                            />
+                        </div>
+                        <div className="relative w-full max-w-sm">
+                            <label>Day</label>
+                            <Button
+                                className="rounded-[4px] border-2 border-gray-100 justify-between text-gray-800 hover:text-gray-800 hover:bg-gray-200 shadow-md bg-transparent w-full"
+                                onClick={() => setopenAddDrop(!openAddDrop)} // toggle dropdown
+                                type="button"
+                            >
+                                {day ? (DaysList.find(d => d.value === day)?.label || "Select day") : "Select day"}
+                                <svg
+                                    viewBox="0 0 16 16"
+                                    fill="currentColor"
+                                    aria-hidden="true"
+                                    className="size-5 text-gray-400 sm:size-4"
+                                >
+                                    <path
+                                        d="M5.22 10.22a.75.75 0 0 1 1.06 0L8 11.94l1.72-1.72a.75.75 0 1 1 1.06 1.06l-2.25 2.25a.75.75 0 0 1-1.06 0l-2.25-2.25a.75.75 0 0 1 0-1.06ZM10.78 5.78a.75.75 0 0 1-1.06 0L8 4.06 6.28 5.78a.75.75 0 0 1-1.06-1.06l2.25-2.25a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06Z"
+                                        clipRule="evenodd"
+                                        fillRule="evenodd"
+                                    />
+                                </svg>
+                            </Button>
+
+                            {openAddDrop && (
+                                <ul className="absolute text-gray-700 pt-1 shadow-md w-full rounded-[4px] bg-gray-200 z-10">
+                                    {DaysList.map((d) => (
+                                        <li key={d.value}>
+                                            <button
+                                                type="button"
+                                                onClick={() => {
+                                                    setDay(d.value);
+                                                    setopenAddDrop(false);
+                                                }}
+                                                className="hover:bg-gray-300 bg-gray-200 w-full text-left pt-1 px-3 pb-1 rounded-[4px] justify-between items-center flex"
+                                            >
+                                                <p>{d.label}</p>
+                                            </button>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                        <div className="flex gap-4">
+                            <div className="flex-1">
+                                <label>Time (24 Hr format)</label>
+                                <Input className="w-full border rounded p-2"
+                                    placeholder="From"
+                                    value={start_min}
+                                    onChange={(e) => setStart_min(e.target.value)}
+                                />
+                            </div>
+                            <div className="flex-1">
+                                <label className="invisible">To</label>
+                                <Input className="w-full border rounded p-2"
+                                    placeholder="To"
+                                    value={end_min}
+                                    onChange={(e) => setEnd_min(e.target.value)}
+                                />
+                            </div>
+                        </div>
+                        <div>
+                            <div>
+                                <label for="hs-color-input">Color</label> <p className="text-gray-200"></p>
+                                <input type="color"
+                                    class="p-1 h-14 w-full block bg-white border border-gray-200 cursor-pointer rounded disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700"
+                                    id="hs-color-input"
+                                    value={color}
+                                    title="Choose your color"
+                                    onChange={handleColorChange}
+                                ></input>
+                            </div>
+                        </div>
+                        <div className="flex justify-end gap-2 pt-4">
+                            <Button type="button" variant="outline" className="cursor-pointer bg-transparent" onClick={() => setOpenAdd(false)}>
+                                Cancel
+                            </Button>
+                            <Button type="submit" className="cursor-pointer">
+                                Save
+                            </Button>
+                        </div>
+                    </form>
+                </DialogContent>
+            </Dialog>
+            {/* TimeTable Session */}
+            <TimeTableGrid propUserId={user._id} />
         </div>
     )
 }
