66f099daf78c6c1a9fb70178d9594b3720428681
Taam Paramee <taamtera@gmail.com>
2025-11-22 23:29:03 +0700

    finished file upload

--- frontend/context/UserContext.jsx
+++ frontend/context/UserContext.jsx
@@ -1,12 +1,24 @@
 "use client"
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState, useCallback } from "react"
 
 const UserContext = createContext(null)
 
 export function UserProvider({ children, initialUser }) {
 	const [user, setUser] = useState(initialUser)
+
+	const refreshUser = useCallback(async () => {
+		const res = await fetch("http://localhost:3001/api/v1/auth/me", {
+			credentials: "include"
+		})
+
+		const data = await res.json()
+		if (data.status === "success") {
+			setUser(data.user)
+		}
+	}, [])
+
 	return (
-		<UserContext.Provider value={{ user, setUser }}>
+		<UserContext.Provider value={{ user, setUser, refreshUser }}>
 			{children}
 		</UserContext.Provider>
 	)
