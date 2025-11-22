c2339c578697405f3f300242403a401731ec3de8
Oak Soe Htet <erikbartholemew@gmail.com>
2025-11-22 21:49:30 +0700

    fixed minor bug

--- frontend/components/options/server_options/members_tab.jsx
+++ frontend/components/options/server_options/members_tab.jsx
@@ -1,6 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Input } from "@/components/ui/input"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { DropdownMenu,
+    DropdownMenuPortal,
+    DropdownMenuSub,
+    DropdownMenuSubContent,
+    DropdownMenuSubTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 import { Search, Ellipsis } from "lucide-react"
 import { useMemo } from "react"
 import { useEffect, useState } from "react"
